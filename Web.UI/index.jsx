6b5ffa10bada3239f1932b5e9e57bf840f7da0d7
Liqiang Chen <leechen@microsoft.com>
2016-11-14 17:34:10 -0800

    initial

---
+++ Web.UI/index.jsx
@@ -0,0 +1,73 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+import { Router, Route, IndexRoute, IndexLink, Link, hashHistory } from "react-router";
+
+function Home(props) {
+    console.log("Home:");
+    console.log(this);
+    return (
+        <div>
+          <h2>Home</h2>
+          <p>
+              Home stuff
+          </p>
+        </div>
+      );
+};
+
+function Configuration(props) {
+    console.log("Configuration:");
+    console.log(this);
+    return (
+        <div>
+          <h2>Configuration</h2>
+          <p>
+              Configuration stuff.
+          </p>
+        </div>
+      );
+};
+
+function Result(props) {
+    console.log("Result:");
+    console.log(this);
+    return (
+        <div>
+          <h2>Result</h2>
+          <p>
+              Result stuff.
+          </p>
+        </div>
+      );
+};
+
+const App = React.createClass({
+  render: function() {
+    console.log("App:");
+    console.log(this);
+    return (
+        <div>
+            <h1>Lighthouse</h1>
+            <ul className="header">
+                <li><IndexLink to="/" activeClassName="active">Home</Link></li>
+                <li><Link to="/Configuration" activeClassName="active">Configuration</Link></li>
+                <li><Link to="/Result" activeClassName="active">Result</Link></li>
+            </ul>
+            <div className="content">
+                {this.props.children}
+            </div>
+        </div>
+    )
+  }
+});
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+        <IndexRoute component={Home} />
+        <Route path="configuration" component={Configuration} />
+        <Route path="result" component={Result} />
+    </Route>
+  </Router>,
+  document.getElementById('container')
+);
---
+++ Web.UI/webpack.config.js
@@ -0,0 +1,21 @@
+﻿"use strict";
+
+module.exports = {
+    devtool: 'source-map',
+    entry: "./index.jsx",
+    output: {
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                query: {
+                    presets: ["es2015", "react"]
+                }
+            }
+        ]
+    }
+};
